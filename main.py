2782490fcc0b0590916ad91c798fe09277aaad94
kotnid <tkt0506tkt@gmail.com>
2022-04-21 14:20:56 +0800

    tele_chat_hist_bot : Add sticker count to main.py
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- main.py
+++ main.py
@@ -121,5 +121,48 @@ def msg_count(message):
         remove("plot.jpg")
 
 
+# sticker count
+@bot.message_handler(commands="sticker_count")
+def sticker_count(message):
+    f = open('result.json','r',encoding="utf-8")
+    data = json.load(f)
+
+    name = []
+    for msg in data["messages"]:
+        if msg["type"] != "message":
+            continue
+        try:
+            if msg["media_type"] == "sticker":
+                if msg["from"] not in name:
+                    name.append(msg["from"]) 
+        except:
+            pass
+
+    
+    for people in name:
+        df = pandas.DataFrame(data["messages"])
+        df = df.rename(columns={"from":"name"})
+        df = df.query("type == 'message'").query(f"media_type == 'sticker'").query(f"name == '{people}'")
+        df = df[['date']]
+        df['date'] = pandas.to_datetime(df['date'])
+        df['count'] = 1
+        df = df.resample("W", on='date').agg({'count':'sum'}).reset_index()
+        df['sum'] = df['count'].cumsum()
+        plt.plot(df["date"] , df["sum"]  , label=people)
+
+    plt.legend()
+    plt.title('Total used of the stickers')
+    plt.xlabel('time')
+    plt.ylabel('amount')
+    plt.gcf().autofmt_xdate()
+    plt.savefig('plot.jpg')
+    plt.clf()
+    bot.send_photo(message.chat.id , photo=open("plot.jpg" , "rb"))
+
+    sleep(3)
+    if path.exists("plot.jpg"):
+        remove("plot.jpg")
+
+
 # run telebot
 asyncio.run(bot.infinity_polling())
