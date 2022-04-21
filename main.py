32a6d8c0d1bcd6794b41b4835393f1d5919ff3a0
kotnid <tkt0506tkt@gmail.com>
2022-04-21 13:57:30 +0800

    tele_chat_hist_bot : Add upload file func to main.py
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- main.py
+++ main.py
@@ -20,5 +20,24 @@ botToken = config['telegram']['token']
 bot =  TeleBot(botToken,  parse_mode="Markdown")
 
 
+# upload file 
+@bot.message_handler(commands=['upload'])
+def upload(message):
+    bot.register_next_step_handler(bot.reply_to(message , 'Pls upload your json file'), upload_file)
+
+def upload_file(message):
+    if message.document.file_name[-5:] != ".json":
+        bot.reply_to(message , "Wrong file type")
+        return ''
+
+    file_info = bot.get_file(message.document.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open('result.json' , 'wb') as file:
+        file.write(downloaded_file)
+    
+    bot.reply_to(message , "Data updated ! You can check yout chat history by using cmd on that group")
+    
+
+
 # run telebot
 asyncio.run(bot.infinity_polling())
