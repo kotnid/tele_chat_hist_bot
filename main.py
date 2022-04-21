ad61739b64f477896193fdef78674473137d4b27
kotnid <tkt0506tkt@gmail.com>
2022-04-21 13:46:12 +0800

    tele_chat_hist_bot : Add logging setup to main.py
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import configparser
+from logging import info , basicConfig , INFO
 
 
 # config setup
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+
+# logging config 
+basicConfig(level= INFO,
+            format= '%(asctime)s %(levelname)s %(message)s',
+            datefmt= '%Y-%m-%d %H:%M')
